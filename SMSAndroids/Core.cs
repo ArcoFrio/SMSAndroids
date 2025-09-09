737bf84e0e3e43edfd4c535ebbde83d04d878950
ArcoFrio <that.random.e.boy@gmail.com>
2025-09-08 22:46:03 -0300

    0.5.2 - Changed version header

--- SMSAndroids/Core.cs
+++ SMSAndroids/Core.cs
@@ -36,7 +36,7 @@ namespace SMSAndroidsCore
         #region Plugin Info
         public const string pluginGuid = "treboy.starmakerstory.smsandroidscore.core";
         public const string pluginName = "Androids Core";
-        public const string pluginVersion = "0.5.1";
+        public const string pluginVersion = "0.5.2";
         #endregion
 
         public static AssetBundle dialogueBundle;
